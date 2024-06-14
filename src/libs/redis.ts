c9edb09d446f104a987c4de2747674cf3025bfea
arrdix <mail.ardix@gmail.com>
2024-06-14 13:44:28 +0700

    fix: fix redis configs

--- src/libs/redis.ts
+++ src/libs/redis.ts
@@ -1,16 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createClient } from 'redis'
 import { RedisClientType } from '@redis/client'
-
+import { REDIS_URL } from '../configs/config'
 import CircleError from '../utils/CircleError'
-import dotenv from 'dotenv'
-dotenv.config()
 
 export let redisClient: RedisClientType<any, any, any>
 
 export async function initRedis() {
     redisClient = await createClient({
-        url: `${process.env.REDIS_URL}`,
+        url: `${REDIS_URL}`,
     })
         .on('error', (err) => {
             throw new CircleError({ error: `Redis client error: ${err}` })
