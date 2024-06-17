fd126471dd3c3b8500733df75bfec5fbfdaabb16
arrdix <mail.ardix@gmail.com>
2024-06-17 15:43:47 +0700

    service: implement auto-generate email mechanism

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -10,9 +10,12 @@ cloudinary.config({
 
 export const HOST = process.env.HOST
 export const LOCALHOST = process.env.LOCALHOST
+export const CLIENT = process.env.CLIENT
 export const PORT = process.env.PORT
 export const SECRET_SAUCE = process.env.SECRET_SAUCE
 export const SALT_ROUND = process.env.SALT_ROUND
 export const REDIS_URL = process.env.REDIS_URL
+export const MAILER_USERNAME = process.env.MAILER_USERNAME
+export const MAILER_PASSWORD = process.env.MAILER_PASSWORD
 
 export { cloudinary }
